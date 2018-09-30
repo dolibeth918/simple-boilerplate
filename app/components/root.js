ee7a05734dced343293081bcbfa10ac3e3dba203
Dolibeth Estevez <dolibeth.estevez@gmail.com>
2018-09-29 22:17:15 -0400

    remove unnecessary html

--- app/components/root.js
+++ app/components/root.js
@@ -7,8 +7,7 @@ const Root = () => {
         Welcome!
       </nav>
       <main>
-        <h1>Welcome to the Margaret Hamilton Academy of JavaScript!</h1>
-        <p>This seems like a nice place to get started with some Routes!</p>
+        <h1>Welcome to Simple Boilerplate!</h1>
       </main>
     </div>
   )
